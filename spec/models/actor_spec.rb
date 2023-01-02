22fda10cd70132b7fa5eaa9d6d8694d6ea44594f
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-02 08:27:42 -0800

    Test/update: actor relationships

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Actor do
   describe 'associations' do
     it {should have_many :actor_movies}
-    it {should have_many(:movies).through(:employee_tickets)}
+    it {should have_many(:movies).through(:actor_movies)}
   end
 end
