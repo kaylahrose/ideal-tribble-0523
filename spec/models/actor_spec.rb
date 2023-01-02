28b701285483c084823dbac01f548203ad556e4b
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-02 09:39:21 -0800

    Test: order actors by age

--- spec/models/actor_spec.rb
+++ spec/models/actor_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Actor do
   describe 'associations' do
-    it {should have_many :actor_movies}
-    it {should have_many(:movies).through(:actor_movies)}
+    it { should have_many :actor_movies }
+    it { should have_many(:movies).through(:actor_movies) }
+  end
+
+  describe 'class methods' do
+    describe '#ordered_ages from youngest to oldest' do
+      james = Actor.create!(name: 'James Cameron', age: 74)
+      andre = Actor.create!(name: 'Andre Botkin', age: 36)
+      mariah = Actor.create!(name: 'Mariah Ahmed', age: 33)
+
+      expect(Actor.ordered_ages).to eq([mariah, andre, james])
+    end
   end
 end
