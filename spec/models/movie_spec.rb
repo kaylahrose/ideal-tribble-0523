456fb1300b53827e339cbb467bf9e5516d3381ad
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-02 10:04:45 -0800

    Test: average age of a movies actors

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -20,10 +20,29 @@ RSpec.describe Movie do
         andre = Actor.create!(name: 'Andre Botkin', age: 36)
         mariah = Actor.create!(name: 'Mariah Ahmed', age: 33)
         ActorMovie.create!(actor_id: james.id, movie_id: spirited_away.id)
-    ActorMovie.create!(actor_id: andre.id, movie_id: spirited_away.id)
-    ActorMovie.create!(actor_id: mariah.id, movie_id: spirited_away.id)
+        ActorMovie.create!(actor_id: andre.id, movie_id: spirited_away.id)
+        ActorMovie.create!(actor_id: mariah.id, movie_id: spirited_away.id)
 
         expect(spirited_away.ordered_actors).to eq([mariah, andre, james])
+      end
+    end
+
+    describe '#actors_avg_age' do
+      it 'returns the average age of a movies actors' do
+        Studio.destroy_all
+        Movie.destroy_all
+        Actor.destroy_all
+
+        studio_1 = Studio.create!(name: 'Gibbly', location: 'Japan')
+        spirited_away = studio_1.movies.create!(title: 'Spirited Away', creation_year: 1998, genre: 'Animation')
+        james = Actor.create!(name: 'James Cameron', age: 74)
+        andre = Actor.create!(name: 'Andre Botkin', age: 36)
+        mariah = Actor.create!(name: 'Mariah Ahmed', age: 33)
+        ActorMovie.create!(actor_id: james.id, movie_id: spirited_away.id)
+        ActorMovie.create!(actor_id: andre.id, movie_id: spirited_away.id)
+        ActorMovie.create!(actor_id: mariah.id, movie_id: spirited_away.id)
+
+        expect(spirited_away.actors_avg_age).to eq(48)
       end
     end
   end
