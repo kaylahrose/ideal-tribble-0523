ce1f294ef752379d563495c313326345fec121cb
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2023-01-02 08:54:40 -0800

    Test: studio index lists movies

--- spec/features/studios/index_spec.rb
+++ spec/features/studios/index_spec.rb
@@ -11,9 +11,9 @@ RSpec.describe 'the studios index' do
   # including the movie's title, creation year, and genre
 
   it 'lists each studios name and location' do
-    studio_1 = Studio.create(name: "Gibbly", location: "Japan")
-    studio_2 = Studio.create(name: "Alpha", location: "South Africa")
-    studio_3 = Studio.create(name: "Beta", location: "Canada")
+    studio_1 = Studio.create!(name: 'Gibbly', location: 'Japan')
+    studio_2 = Studio.create!(name: 'Alpha', location: 'South Africa')
+    studio_3 = Studio.create!(name: 'Beta', location: 'Canada')
 
     visit '/studios'
 
@@ -24,6 +24,26 @@ RSpec.describe 'the studios index' do
     expect(page).to have_content(studio_3.name)
     expect(page).to have_content(studio_3.location)
   end
-  
-  it 'lists all stuidos movies under the studio including title, creation, year, and genre'
+
+  it 'lists all stuidos movies under the studio including title, creation, year, and genre' do
+    studio_1 = Studio.create!(name: 'Gibbly', location: 'Japan')
+    studio_2 = Studio.create!(name: 'Alpha', location: 'South Africa')
+    studio_3 = Studio.create!(name: 'Beta', location: 'Canada')
+
+    spirited_away = studio_1.movies.create!(title: 'Spirited Away', creation_year: 1998, genre: 'Animation')
+    castles = studio_1.movies.create!(title: 'Castles In The Sky', creation_year: 2003, genre: 'Animation')
+    runner = studio_2.movies.create!(title: 'Runner', creation_year: 2005, genre: 'Thriller')
+
+    visit '/studios'
+
+    within("#studio-#{studio_1.id}") do
+      expect(page).to have_content(spirited_away.title)
+      expect(page).to have_content(spirited_away.creation_year)
+      expect(page).to have_content(spirited_away.genre)
+      expect(page).to have_content(castles.title)
+      expect(page).to have_content(castles.creation_year)
+      expect(page).to have_content(castles.genre)
+      expect(page).to have_no_content(runner.genre)
+    end
+  end
 end
